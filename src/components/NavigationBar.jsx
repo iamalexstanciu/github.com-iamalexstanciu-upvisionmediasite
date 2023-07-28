fdb6d93e790e7916adb1f8353413d4fd6d5d3460
iamalexstanciu <alexstanciu@outlook.it>
2023-07-28 11:22:18 +0300

    Solved navbar problem with hamburger.

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -4,6 +4,19 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function NavigationBar() {
   const [isSticky, setIsSticky] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -29,49 +42,70 @@ function NavigationBar() {
     };
   }, []);
 
+  const openMenu = () => {
+    setMenuIsOpen(!menuIsOpen);
+  };
+
   return (
     <>
-      <div
-        className={`navbar navbar-inverse navbar-fixed-top ${
-          isSticky ? "sticky" : ""
-        }`}
-        role="navigation">
-        <div className="container">
-          <div className="navbar-header">
-            <button
-              type="button"
-              className="navbar-toggle"
-              data-toggle="collapse"
-              data-target=".navbar-collapse">
-              <span className="sr-only">Toggle navigation</span>
-              <span className="icon-bar" />
-              <span className="icon-bar" />
-              <span className="icon-bar" />
-            </button>
-            <div className="small-logo-container">
-              <a className="small-logo" href="#">
-                ↥ upVisionMedia
-              </a>
-            </div>
-          </div>
-          <div className="navbar-collapse collapse">
-            <ul className="nav navbar-nav navbar-right">
-              <li className="active">
-                <a href="#">Active</a>
-              </li>
-              <li>
-                <a href="#">Link</a>
-              </li>
-              <li>
-                <a href="#">Link</a>
-              </li>
-              <li>
-                <a href="#">Link</a>
-              </li>
+      {isMobile ? (
+        <div className={`mobile-navbar ${menuIsOpen ? "open" : ""}`}>
+          <button className="navbar-toggle" onClick={openMenu}>
+            {" "}
+            |||
+          </button>
+          <div className="menu-items">
+            <ul className="menu-items-links">
+              <a href="/services">Services</a>
+              <a href="/clients">Clients</a>
+              <a href="/contact">Contact</a>
+              <a href="/about">About Us</a>
             </ul>
           </div>
         </div>
-      </div>
+      ) : (
+        <div
+          className={`navbar navbar-inverse navbar-fixed-top ${
+            isSticky ? "sticky" : ""
+          }`}
+          role="navigation">
+          <div className="container">
+            <div className="navbar-header">
+              <button
+                type="button"
+                className="navbar-toggle"
+                data-toggle="collapse"
+                data-target=".navbar-collapse">
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar" />
+                <span className="icon-bar" />
+                <span className="icon-bar" />
+              </button>
+              <div className="small-logo-container">
+                <a className="small-logo" href="/home">
+                  ↥ upVisionMedia
+                </a>
+              </div>
+            </div>
+            <div className="navbar-collapse collapse">
+              <ul className="nav navbar-nav navbar-right">
+                <li className="active">
+                  <a href="/services">Services</a>
+                </li>
+                <li>
+                  <a href="/clients">Clients</a>
+                </li>
+                <li>
+                  <a href="/contact">Contact</a>
+                </li>
+                <li>
+                  <a href="/about">About Us</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      )}
 
       <div className="container-fluid big-logo-row">
         <div className="container">
