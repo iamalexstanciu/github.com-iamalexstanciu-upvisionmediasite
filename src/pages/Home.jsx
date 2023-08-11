7b8b21559a02e6f0a00c435a7c820be3b7e20fe8
iamalexstanciu <alexstanciu@outlook.it>
2023-08-11 11:25:40 +0300

    Design test homepage.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,41 +1,38 @@
 import React from "react";
 import "../styling/Home.css";
-import { Link } from "react-router-dom";
-import HomeBannerImage from "../assets/upvisionMediaHomePageBanner2.png";
+import HomeBannerImage from "../assets/home.png";
 import { Typewriter } from "react-simple-typewriter";
 
 function Home() {
   return (
     <div className="container-home">
       <div className="left-side-home">
-        <h1 className="animatedTextHome">
-          <Typewriter
-            words={[
-              "Welcome to upvisionMedia.",
-              "Your business, our priority.",
-              "Unleash Your Digital Potential.",
-            ]}
-            loop={200}
-            cursor
-            cursorStyle="|"
-            typeSpeed={200}
-            deleteSpeed={50}
-            delaySpeed={1000}
-          />
-        </h1>
-        <h4 className="h4TextHomepage">
-          Ready to take your web presence to new heights?
-        </h4>
-        <h4 className="h4TextHomepage">
-          Collaborate with us for cutting-edge web solutions.
-        </h4>
-        <Link to="/contact">
-          <div className="container2">
-            <div className="neumor">
-              <p></p>
-            </div>
-          </div>
-        </Link>
+        <div className="text-content">
+          <h1 className="animatedTextHome">
+            <Typewriter
+              words={[
+                "Welcome to upvisionMedia.",
+                "Your business, our priority.",
+                "Unleash Your Digital Potential.",
+              ]}
+              loop={200}
+              cursor
+              cursorStyle="|"
+              typeSpeed={200}
+              deleteSpeed={50}
+              delaySpeed={1000}
+            />
+          </h1>
+          <h4 className="h4TextHomepage">
+            Ready to take your web presence to new heights?
+          </h4>
+          <h4 className="h4TextHomepage">
+            Collaborate with us for cutting-edge web solutions.
+          </h4>
+        </div>
+        <div className="circle-button">
+          <span className="arrow"></span>
+        </div>
       </div>
       <div className="right-side-home">
         <img
