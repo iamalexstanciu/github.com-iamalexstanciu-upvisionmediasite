70304e93d9ebf52bc70fc487bba257e9c8ad7834
iamalexstanciu <alexstanciu@outlook.it>
2023-08-20 18:46:59 +0200

    Working on services.

--- src/pages/services/ContentPage1.jsx
+++ src/pages/services/ContentPage1.jsx
@@ -4,44 +4,51 @@ import "../../styling/Page1.css";
 function ContentPage1() {
   return (
     <div className="page1-container">
-      <img
-        className="page1-image"
-        src="https://images.unsplash.com/photo-1521737711867-e3b97375f902?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
-        alt="services first pic agency"
-      />
-      <div className="second-column-page1">
-        <h2>
-          Unveil your business potential with captivating online presence!
-        </h2>
-        <div className="statistics-online-presence">
-          <h3>Importance of Online Presence for Businesses</h3>
-          <div className="first-stats">
-            <h4>Increased Visibility</h4>
-            <p>
-              Having a strong online presence provides businesses with a
-              platform to gain visibility and reach a larger audience. With the
-              right resources and promotion, your company can be easily seen by
-              potential customers and those interested in your products or
-              services.
-            </p>
+      <h2 className="title-page1-services">
+        Unveil your business potential with captivating online presence!
+      </h2>
+      <div className="card-container-services-page1">
+        <div class="card-services-page1">
+          <div class="card__content">
+            Increased Visibility
+            <div>
+              <p className="card_content-p">
+                {" "}
+                Having a strong online presence provides businesses with a
+                platform to gain visibility and reach a larger audience. With
+                the right resources and promotion, your company can be easily
+                seen by potential customers and those interested in your
+                products or services.
+              </p>
+            </div>
           </div>
-          <div className="second-stats">
-            <h4>Increased Brand Awareness</h4>
-            <p>
-              Having an online presence allows businesses to reach more people
-              and build a stronger brand awareness. With the right content and
-              promotion, your business can be seen by more potential customers
-              and build a more recognizable brand.
-            </p>
+        </div>
+        <div class="card-services-page1">
+          <div class="card__content">
+            Increased Brand Awareness
+            <div>
+              <p className="card_content-p">
+                {" "}
+                Having an online presence allows businesses to reach more people
+                and build a stronger brand awareness. With the right content and
+                promotion, your business can be seen by more potential customers
+                and build a more recognizable brand.
+              </p>
+            </div>
           </div>
-          <div className="third-stats">
-            <h4>Increased Trust</h4>
-            <p>
-              People are more likely to trust a business that is visible online.
-              An online presence allows businesses to establish a reputation and
-              a level of trust with customers, helping to drive sales and gain
-              new customers.
-            </p>
+        </div>
+        <div class="card-services-page1">
+          <div class="card__content">
+            Increased Trust
+            <div>
+              <p className="card_content-p">
+                {" "}
+                People are more likely to trust a business that is visible
+                online. An online presence allows businesses to establish a
+                reputation and a level of trust with customers, helping to drive
+                sales and gain new customers.
+              </p>
+            </div>
           </div>
         </div>
       </div>
