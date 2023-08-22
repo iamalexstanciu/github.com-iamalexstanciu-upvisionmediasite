95d0f3bddfd97a075b67aed263a35d135e1cd3b8
iamalexstanciu <alexstanciu@outlook.it>
2023-08-22 11:58:26 +0200

    Services

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "../styling/Services.css";
-import GifServices from "../assets/gif-page2.gif";
+import ServicesImage from "../assets/services-image.png";
 
 export default function Services() {
   return (
     <div className="hero">
       <div className="services-left-side">
-        <img className="services-gif" src={GifServices} alt="presentation" />
+        <img className="services-image" src={ServicesImage} alt="presentation" />
       </div>
       <div className="services-right-side">
         <h3 className="title-right-side">Our services</h3>
