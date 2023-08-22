3ef03999ed44206c2cc576bccab1e16e3cad60d3
iamalexstanciu <alexstanciu@outlook.it>
2023-08-22 11:48:23 +0200

    Working on services

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
+import "../styling/Services.css";
+import GifServices from "../assets/gif-page2.gif";
 
 export default function Services() {
   return (
-    <div>Services</div>
-  )
+    <div className="hero">
+      <div className="services-left-side">
+        <img className="services-gif" src={GifServices} alt="presentation" />
+      </div>
+      <div className="services-right-side">
+        <h3 className="title-right-side">Our services</h3>
+        <ul>
+          <li>Search Engine Optimization</li>
+          <li>Mobile Responsiveness</li>
+          <li>Landing Pages</li>
+          <li>Branding</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
