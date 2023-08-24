d44d23b6f4fe29ae9769b2ad32ed27e8e24adda1
iamalexstanciu <alexstanciu@outlook.it>
2023-08-24 15:14:46 +0200

    Contact done

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,111 @@
-import React from 'react'
+import React, { useState } from "react";
+import * as emailjs from "@emailjs/browser";
+import "../styling/Contact.css";
+import { contactConfig } from "../js/data";
 
 export default function Contact() {
+  const [formData, setFormData] = useState({
+    email: "",
+    name: "",
+    message: "",
+    loading: false,
+    show: false,
+    alertMessage: "",
+    variant: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormData({ loading: true });
+
+    const templateParams = {
+      from_name: formData.email,
+      user_name: formData.name,
+      to_name: contactConfig.YOUR_EMAIL,
+      message: formData.message,
+    };
+
+    emailjs
+      .send(
+        contactConfig.YOUR_SERVICE_ID,
+        contactConfig.YOUR_TEMPLATE_ID,
+        templateParams,
+        contactConfig.YOUR_USER_ID
+      )
+      .then(
+        (res) => {
+          console.log("🚀 ~ file: Contact.jsx:36 ~ handleSubmit ~ res:", res);
+          setFormData({
+            loading: false,
+            alertMessage:
+              "Thank you for the message, we'll answer as soon as possible! Have a nice day!",
+            variant: "success",
+            show: true,
+          });
+        },
+        (error) => {
+          console.log(error.text);
+          setFormData({
+            alertMessage: `Failed to send!, ${error.message}`,
+            variant: "error",
+            show: true,
+          });
+        }
+      );
+  };
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
-    <div id='contact'>
-       <h1 style={{color:"green", textAlign: "center", fontSize: 55, paddingTop: "5%"}}>This is Contact Section</h1>
+    <div className="contact-section" id="contact">
+      <div className="let-s-talk">Let’s talk</div>
+      <div className="description">
+        Do you want to take your business to another level by having 100% online
+        exposure? Write to us and we will get in touch to find the best solution
+        made especially for you.
+      </div>
+
+      <div className="form-container">
+        <form className="contact-form" onSubmit={handleSubmit}>
+          <label>Name</label>
+          <input
+            id="name"
+            name="name"
+            placeholder="You name"
+            value={formData.name || ""}
+            type="text"
+            required
+            onChange={handleChange}
+          />
+          <label>Email</label>
+          <input
+            id="email"
+            name="email"
+            placeholder="Please, your email address"
+            type="email"
+            value={formData.email || ""}
+            required
+            onChange={handleChange}
+          />
+          <label>Message</label>
+          <textarea
+            id="message"
+            name="message"
+            placeholder="Let's get in touch "
+            rows="5"
+            value={formData.message}
+            onChange={handleChange}
+            required></textarea>
+          <button className="submitButton" type="submit">
+            {formData.loading ? "Sending..." : "Send"}
+          </button>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
