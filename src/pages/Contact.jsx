7035cbe9686bf353461687a4a67e56e24bbbe906
iamalexstanciu <alexstanciu@outlook.it>
2023-09-14 16:27:32 +0300

    Changed buttons.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -107,7 +107,21 @@ export default function Contact() {
           </div>
           <div class="form-field col x-100 align-center">
             <button className="submit-btn" type="submit" value="Submit">
-              {formData.loading ? "Sending..." : "Send"}
+              <div class="svg-wrapper-1">
+                <div class="svg-wrapper">
+                  <svg
+                    height="24"
+                    width="24"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path d="M0 0h24v24H0z" fill="none"></path>
+                    <path
+                      d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"
+                      fill="currentColor"></path>
+                  </svg>
+                </div>
+              </div>
+              <span>{formData.loading ? "Sending..." : "Send"}</span>
             </button>
           </div>
         </form>
