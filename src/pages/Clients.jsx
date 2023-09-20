196ca48cb5a1259e4121e04d7d911328d2da1d11
iamalexstanciu <alexstanciu@outlook.it>
2023-09-20 15:47:18 +0300

    Temporary solved bug clients.

--- src/pages/Clients.jsx
+++ src/pages/Clients.jsx
@@ -8,111 +8,109 @@ import Project5 from "../assets/project3.jpg";
 import Project6 from "../assets/project3.1.jpg";
 
 function Clients() {
-  const [mouseDownAt, setMouseDownAt] = useState(-45);
-  const [prevPercentage, setPrevPercentage] = useState(0);
+  // const [mouseDownAt, setMouseDownAt] = useState(45);
+  // const [prevPercentage, setPrevPercentage] = useState(-45);
 
-  const trackRef = useRef(null);
+  // const trackRef = useRef(null);
 
-  const handleOnDown = (e) => setMouseDownAt(e.clientX);
+  // const handleOnDown = (e) => setMouseDownAt(e.clientX);
 
-  const handleOnUp = () => {
-    setMouseDownAt(0);
-    //Added this 2 lines to fix the Nan issue of trackRef.current.dataset.percentage;
-    //setPrevPercentage(trackRef.current.dataset.percentage); replaced
-    const newPercentage = trackRef.current.dataset.percentage;
-    setPrevPercentage(
-      isNaN(parseFloat(newPercentage)) ? 0 : parseFloat(newPercentage)
-    );
-  };
+  // const handleOnUp = () => {
+  //   setMouseDownAt(45);
+  //   const newPercentage = trackRef.current.dataset.percentage;
+  //   setPrevPercentage(
+  //     isNaN(parseFloat(newPercentage)) ? 0 : parseFloat(newPercentage)
+  //   );
+  // };
 
-  const handleOnMove = (e) => {
-    if (mouseDownAt === 0) return;
-    // console.log({ mouseDownAt, prevPercentage });
-    const mouseDelta = parseFloat(mouseDownAt) - e.clientX;
-    const maxDelta = window.innerWidth / 2;
+  // const handleOnMove = (e) => {
+  //   if (mouseDownAt === 0) return;
+  //   // console.log({ mouseDownAt, prevPercentage });
+  //   const mouseDelta = parseFloat(mouseDownAt) - e.clientX;
+  //   const maxDelta = window.innerWidth / 2;
 
-    const percentage = (mouseDelta / maxDelta) * -100;
-    const nextPercentageUnconstrained = parseFloat(prevPercentage) + percentage;
-    const nextPercentage = Math.max(
-      Math.min(nextPercentageUnconstrained, 0),
-      0
-    );
+  //   const percentage = (mouseDelta / maxDelta) * -100;
+  //   const nextPercentageUnconstrained = parseFloat(prevPercentage) + percentage;
+  //   const nextPercentage = Math.max(
+  //     Math.min(nextPercentageUnconstrained, 0),
+  //     0
+  //   );
 
-    trackRef.current.dataset.percentage = nextPercentage;
+  //   trackRef.current.dataset.percentage = nextPercentage;
 
-    trackRef.current.animate(
-      {
-        transform: `translate(${nextPercentage}%, -50%)`,
-      },
-      { duration: 1200, fill: "forwards" }
-    );
+  //   trackRef.current.animate(
+  //     {
+  //       transform: `translate(${nextPercentage}%, -50%)`,
+  //     },
+  //     { duration: 1200, fill: "forwards" }
+  //   );
 
-    const images = trackRef.current.getElementsByClassName("image");
-    for (const image of images) {
-      image.animate(
-        {
-          objectPosition: `${100 + nextPercentage}% center`,
-        },
-        { duration: 1200, fill: "forwards" }
-      );
-    }
-  };
+  //   const images = trackRef.current.getElementsByClassName("image");
+  //   for (const image of images) {
+  //     image.animate(
+  //       {
+  //         objectPosition: `${100 + nextPercentage}% center`,
+  //       },
+  //       { duration: 1200, fill: "forwards" }
+  //     );
+  //   }
+  // };
 
-  useEffect(() => {
-    window.addEventListener("mousedown", handleOnDown);
-    window.addEventListener("touchstart", handleOnDown);
-    window.addEventListener("mouseup", handleOnUp);
-    window.addEventListener("touchend", handleOnUp);
-    window.addEventListener("mousemove", handleOnMove);
-    window.addEventListener("touchmove", handleOnMove);
+  // useEffect(() => {
+  //   window.addEventListener("mousedown", handleOnDown);
+  //   window.addEventListener("touchstart", handleOnDown);
+  //   window.addEventListener("mouseup", handleOnUp);
+  //   window.addEventListener("touchend", handleOnUp);
+  //   window.addEventListener("mousemove", handleOnMove);
+  //   window.addEventListener("touchmove", handleOnMove);
 
-    return () => {
-      window.removeEventListener("mousedown", handleOnDown);
-      window.removeEventListener("touchstart", handleOnDown);
-      window.removeEventListener("mouseup", handleOnUp);
-      window.removeEventListener("touchend", handleOnUp);
-      window.removeEventListener("mousemove", handleOnMove);
-      window.removeEventListener("touchmove", handleOnMove);
-    };
-  });
+  //   return () => {
+  //     window.removeEventListener("mousedown", handleOnDown);
+  //     window.removeEventListener("touchstart", handleOnDown);
+  //     window.removeEventListener("mouseup", handleOnUp);
+  //     window.removeEventListener("touchend", handleOnUp);
+  //     window.removeEventListener("mousemove", handleOnMove);
+  //     window.removeEventListener("touchmove", handleOnMove);
+  //   };
+  // });
 
-  //intersection observer
-  const sectionRef = useRef(null);
+  // //intersection observer
+  // const sectionRef = useRef(null);
 
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.isIntersecting) {
-          const images = entry.target.getElementsByClassName("image");
-          for (const image of images) {
-            image.classList.add("animate");
-          }
-        } else {
-          const images = entry.target.getElementsByClassName("image");
-          for (const image of images) {
-            image.classList.remove("animate");
-          }
-        }
-      },
-      {
-        root: null,
-        rootMargin: "0px",
-        threshold: 0.1,
-      }
-    );
+  // useEffect(() => {
+  //   const observer = new IntersectionObserver(
+  //     ([entry]) => {
+  //       if (entry.isIntersecting) {
+  //         const images = entry.target.getElementsByClassName("image");
+  //         for (const image of images) {
+  //           image.classList.add("animate");
+  //         }
+  //       } else {
+  //         const images = entry.target.getElementsByClassName("image");
+  //         for (const image of images) {
+  //           image.classList.remove("animate");
+  //         }
+  //       }
+  //     },
+  //     {
+  //       root: null,
+  //       rootMargin: "0px",
+  //       threshold: 0.1,
+  //     }
+  //   );
 
-    const sectionRefCurrent = sectionRef.current; // Capture the current value of sectionRef
+  //   const sectionRefCurrent = sectionRef.current; // Capture the current value of sectionRef
 
-    if (sectionRefCurrent) {
-      observer.observe(sectionRefCurrent);
-    }
+  //   if (sectionRefCurrent) {
+  //     observer.observe(sectionRefCurrent);
+  //   }
 
-    return () => {
-      if (sectionRefCurrent) {
-        observer.unobserve(sectionRefCurrent);
-      }
-    };
-  }, []);
+  //   return () => {
+  //     if (sectionRefCurrent) {
+  //       observer.unobserve(sectionRefCurrent);
+  //     }
+  //   };
+  // }, []);
 
   return (
     <div className="clients-section" id="clients">
@@ -121,12 +119,10 @@ function Clients() {
         These are some examples of projects in which we have put all our
         professionalism and passion.
       </div>
-      <div className="projects-container" ref={sectionRef}>
+      <div className="projects-container">
         <div
           id="image-track"
-          ref={trackRef}
-          data-mouse-down-at={mouseDownAt}
-          data-prev-percentage={prevPercentage}>
+         >
           <img
             className="image"
             src={Project1}
