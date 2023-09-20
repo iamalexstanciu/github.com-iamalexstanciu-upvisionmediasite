23b06c5b8865b50878e0d585c6ae59f82f263d6b
iamalexstanciu <alexstanciu@outlook.it>
2023-09-20 11:28:46 +0300

    Working on about page.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -23,43 +23,47 @@ export default function About() {
               <p class="text-blk name">Razvan Tache</p>
               <p class="text-blk position">CEO</p>
               <img class="team-member-image" src={Razvan} alt="presentation" />
-              <a
-                href="https://www.twitter.com"
-                target="_blank"
-                rel="noreferrer">
-                <img
-                  class="social-media-icon"
-                  alt="account"
-                  src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-twitter.svg"
-                />
-              </a>
-              <a
-                href="https://www.facebook.com"
-                target="_blank"
-                rel="noreferrer">
-                <img
-                  class="social-media-icon"
-                  alt="account"
-                  src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-fb.svg"
-                />
-              </a>
-              <a href="https://www.gmail.com" target="_blank" rel="noreferrer">
-                <img
-                  class="social-media-icon"
-                  alt="account"
-                  src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-mail.svg"
-                />
-              </a>
-              <a
-                href="https://www.instagram.com"
-                target="_blank"
-                rel="noreferrer">
-                <img
-                  class="social-media-icon"
-                  alt="account"
-                  src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-insta.svg"
-                />
-              </a>
+
+                <a
+                  href="https://www.twitter.com"
+                  target="_blank"
+                  rel="noreferrer">
+                  <img
+                    class="social-media-icon"
+                    alt="account"
+                    src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-twitter.svg"
+                  />
+                </a>
+                <a
+                  href="https://www.facebook.com"
+                  target="_blank"
+                  rel="noreferrer">
+                  <img
+                    class="social-media-icon"
+                    alt="account"
+                    src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-fb.svg"
+                  />
+                </a>
+                <a
+                  href="https://www.gmail.com"
+                  target="_blank"
+                  rel="noreferrer">
+                  <img
+                    class="social-media-icon"
+                    alt="account"
+                    src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-mail.svg"
+                  />
+                </a>
+                <a
+                  href="https://www.instagram.com"
+                  target="_blank"
+                  rel="noreferrer">
+                  <img
+                    class="social-media-icon"
+                    alt="account"
+                    src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-insta.svg"
+                  />
+                </a>
             </div>
             <div class="responsive-cell-block wk-desk-4 wk-ipadp-4 wk-mobile-12 wk-tab-12 card-container">
               <p class="text-blk name">Alexandru Stanciu</p>
